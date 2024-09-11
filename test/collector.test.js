04b8197d31faf4807656c68733cdf2f53dad65eb
Oliver Huckfeldt <oliverhuckfeldt@gmail.com>
2024-09-11 09:23:56 +0200

    Test the number of mock object calls. Now test the application and apply all methods separately. Now test for multiple handlers with same url. Clear socket mock.

--- test/collector.test.js
+++ test/collector.test.js
@@ -29,63 +29,110 @@ beforeEach(() => {
 
 afterEach(() => {
     core.logger.info.mockClear();
+    socket.send.mockClear();
 });
 
-describe('Handler creation from registry', () => {    
+describe('Successful handler creation', () => {    
     test('All handler created', () => {
-        expect(collector._handler.length).toBe(Object.keys(registry).length);
+        expect(collector._handler).toHaveLength(Object.keys(registry).length);
     });
 
-    test('Handler URL matches URL in registry', () => {
-        collector._handler.forEach(handler => {
-            expect(Object.keys(registry)).toContain(handler.url);
+    test('All registered URLs present', () => {
+        Object.keys(registry).forEach(url => {
+            expect(collector._handler.some(h => h.url === url))
         });
     });
-
-    test('Handler contains collector reference', () => {
+    
+    test('All handlers have a collector reference', () => {
         collector._handler.forEach(handler => {
             expect(handler.collector).toBe(collector);
         });
+    });
+    
+    test('Multiple handlers with same URL', () => {
+        collector._createHandler('/foo', socket, core);
+        collector._createHandler('/foo', socket, core);
+
+        const fooHandlers = collector._handler.filter(h => h.url === '/foo')
+        expect(fooHandlers).toHaveLength(3);
     });
 });
 
 describe('Handler methods', () => {
     test('Call constructor', () => {
+        expect(core.logger.info.mock.calls).toHaveLength(6);
         expect(core.logger.info.mock.calls[0][0]).toBe('/foo handler created.');
         expect(core.logger.info.mock.calls[2][0]).toBe('/bar handler created.');
         expect(core.logger.info.mock.calls[4][0]).toBe('/baz handler created.');
     });
 
     test('Call onConnect', () => {
+        expect(core.logger.info.mock.calls).toHaveLength(6);
         expect(core.logger.info.mock.calls[1][0]).toBe('/foo handler connected.');
         expect(core.logger.info.mock.calls[3][0]).toBe('/bar handler connected.');
         expect(core.logger.info.mock.calls[5][0]).toBe('/baz handler connected.');
     });
 
     test('Call onMessage', () => {
-        collector.applyAll(handler => {
+        collector._handler.forEach(handler => {
             handler.onMessage(`${handler.url} handler message received.`, core);
         });
+        expect(core.logger.info.mock.calls).toHaveLength(9);
         expect(core.logger.info.mock.calls[6][0]).toBe('/foo handler message received.');
         expect(core.logger.info.mock.calls[7][0]).toBe('/bar handler message received.');
         expect(core.logger.info.mock.calls[8][0]).toBe('/baz handler message received.');
     });
 
     test('Call writeMessage', () => {
-        collector.applyAll(handler => {
+        collector._handler.forEach(handler => {
             handler.writeMessage(`${handler.url} handler message send.`);
         });
-        expect(socket.send.mock.calls[0][0]).toBe('/foo handler message send.')
-        expect(socket.send.mock.calls[1][0]).toBe('/bar handler message send.')
-        expect(socket.send.mock.calls[2][0]).toBe('/baz handler message send.')
+        expect(socket.send.mock.calls).toHaveLength(3);
+        expect(socket.send.mock.calls[0][0]).toBe('/foo handler message send.');
+        expect(socket.send.mock.calls[1][0]).toBe('/bar handler message send.');
+        expect(socket.send.mock.calls[2][0]).toBe('/baz handler message send.');
     });
 
     test('Call onClose', () => {
-        collector.applyAll(handler => {
+        collector._handler.forEach(handler => {
             handler.onClose(core);
         });
+        expect(core.logger.info.mock.calls).toHaveLength(9);
         expect(core.logger.info.mock.calls[6][0]).toBe('/foo handler closed.');
         expect(core.logger.info.mock.calls[7][0]).toBe('/bar handler closed.');
         expect(core.logger.info.mock.calls[8][0]).toBe('/baz handler closed.');
+    });
+});
+
+describe('Collector API', () => {
+    test('Use applyAll', () => {
+        collector.applyAll(handler => {
+            handler.writeMessage(`${handler.url} handler called by applyAll.`);
+        });
+        expect(socket.send.mock.calls).toHaveLength(3);
+        expect(socket.send.mock.calls[0][0]).toBe('/foo handler called by applyAll.');
+        expect(socket.send.mock.calls[1][0]).toBe('/bar handler called by applyAll.');
+        expect(socket.send.mock.calls[2][0]).toBe('/baz handler called by applyAll.');
+    });
+
+    test('Use applyOn', () => {
+        collector.applyOn('/foo', handler => {
+            handler.writeMessage(`${handler.url} handler called by applyOn.`);
+        });
+        expect(socket.send.mock.calls).toHaveLength(1);
+        expect(socket.send.mock.calls[0][0]).toBe('/foo handler called by applyOn.');
+    });
+
+    test('Use applyOn multiple', () => {
+        collector._createHandler('/foo', socket, core);
+        collector._createHandler('/foo', socket, core);
+
+        collector.applyOn('/foo', handler => {
+            handler.writeMessage(`${handler.url} handler called by applyOn.`);
+        });
+        expect(socket.send.mock.calls).toHaveLength(3);
+        expect(socket.send.mock.calls[0][0]).toBe('/foo handler called by applyOn.');
+        expect(socket.send.mock.calls[1][0]).toBe('/foo handler called by applyOn.');
+        expect(socket.send.mock.calls[2][0]).toBe('/foo handler called by applyOn.');
     });
 });
